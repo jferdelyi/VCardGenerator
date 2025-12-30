e7e772275950f8f7a92733150a4a30f552c966f1
Jean-François Erdelyi <jferdelyi@gmail.com>
2025-12-30 11:20:43 +0100

    [QA] comment error

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # VCardGenerator
-This small script can generate a valid .vcf (vCard) from CSV file. It will ask you to fill in some details and write the vcf file.
+This small script can generate a valid .vcf (vCard) from CSV file.
--- VCardGenerator.py
+++ VCardGenerator.py
@@ -1,6 +1,5 @@
 """
-This small script can generate a valid .vcf (vCard) from CSV file. It will ask you to fill
-in some details and write the vcf file.
+This small script can generate a valid .vcf (vCard) from CSV file.
 """
 
 import csv
